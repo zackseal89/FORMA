6e96bed477bdd01ce998d8338e2b181d44ec303c
zackseal89 <zacharyongeri121@gmail.com>
2026-05-13 11:50:30 +0300

    Allow cdn.shopify.com in next/image remotePatterns
    Shopify-hosted product images (cdn.shopify.com/s/files/...) were being
    blocked by next/image because the host wasn't in remotePatterns, so every
    ProductCard, gallery, cart line, and PDP image fell back to a broken
    image. Whitelist the Shopify CDN under /s/files/**.

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: "lh3.googleusercontent.com",
         pathname: "/aida-public/**",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        pathname: "/s/files/**",
+      },
     ],
   },
 };
